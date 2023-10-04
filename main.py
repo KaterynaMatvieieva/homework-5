19a12182318931ff2ce2b7644c2c4e4d46ed1b93
Kateryna Matvieieva <kateryna_matvieieva@epam.com>
2023-10-04 21:55:55 +0300

    Added task 1

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
+# У списку цілих, заповненому випадковими числами обчислити:
+# ■ Суму негативних чисел;
+# ■ Суму парних чисел;
+# ■ Суму непарних чисел;
+# ■ Добуток елементів з кратними індексами 3;
+# ■ Добуток елементів між мінімальним та максимальним елементом;
+# ■ Суму елементів, що знаходяться між першим та останнім позитивними елементами.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# import random
+#
+# nums = []
+# NUM_SIZE = 10
+#
+# for i in range(NUM_SIZE):
+#     nums.append(random.randint(-50,50))
+#
+# print(nums)
+#
+# negative_sum = 0
+# even_sum = 0
+# odd_sum = 0
+# product_index3 = 1
+# product_min_max = 1
+# sum_between_positive = 0
+#
+# for number in nums:
+#     if number < 0:
+#         negative_sum += number
+#
+#     if number % 2 == 0:
+#         even_sum += number
+#
+#     if number % 2 != 0:
+#         odd_sum += number
+#
+# min_nums_index = nums.index(min(nums))
+# max_nums_index = nums.index(max(nums))
+#
+# if min_nums_index > max_nums_index:
+#     min_nums_index, max_nums_index = max_nums_index, min_nums_index
+#
+# for i in range(min_nums_index + 1, max_nums_index):
+#     product_min_max = product_min_max * nums[i]
+#
+# first_positive_index = 0
+# last_positive_index = 0
+#
+# for i in range(len(nums)):
+#     if nums[i] > 0:
+#         first_positive_index = i
+#         break
+#
+# for i in range(len(nums) - 1, -1, -1):
+#     if nums[i] > 0:
+#         last_positive_index = i
+#         break
+#
+# for i in range(first_positive_index + 1, last_positive_index):
+#     sum_between_positive += nums[i]
+#
+# for i in range(1,len(nums)):
+#     if i % 3 == 0:
+#         product_index3 = product_index3 * nums[i]
+#
+# print(f"Sum of negative values is : {negative_sum}, sum of even values is: {even_sum}, sum of odd values is {odd_sum}")
+# print(f"Product of elements with /3 indexes is: {product_index3}, product of values between min amd max: {product_min_max}")
+# print(f"Sum of values between the first and the last positive: {sum_between_positive}")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
